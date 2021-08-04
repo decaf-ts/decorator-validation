25fc4c7093d9a3d7660a407f443126cd43c5fb81
Your Name <you@example.com>
2021-08-05 00:09:55 +0100

    refs #1 - update

--- .clean-publish.js
+++ .clean-publish.js
@@ -1,5 +1,19 @@
 module.exports = {
-    "files": [],
+    "files": [
+        ".github",
+        "docs",
+        "build",
+        "workdocs",
+        "jest.config.json",
+        "tests",
+        "jsdocs.json",
+        "mdCompile.json",
+        "nodemon.json",
+        "package-lock.json",
+        "gulpfile.babel.js",
+        ".clean-publish.js",
+        "tsconfig.json"
+    ],
     "packageManager": "npm",
     "access": "public"
 }
