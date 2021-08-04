bc3bdc98362dd3c9bd6150391f17c9b7a796a96d
Your Name <you@example.com>
2021-08-04 02:43:46 +0100

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const {src, dest, parallel} = gulp;
+const {src, dest, parallel, series} = gulp;
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
@@ -7,10 +7,14 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 
+const tsOptions = {
+    "emitDeclarationOnly": true,
+    "sourceMap": false,
+    "declarationMap": false
+};
+
 function exportModules() {
-    const tsProject = ts.createProject('tsconfig.json', {
-        "emitDeclarationOnly": true
-    });
+    const tsProject = ts.createProject('tsconfig.json', tsOptions);
     return src('src/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(tsProject())
@@ -26,9 +30,7 @@ function exportModules() {
 }
 
 function exportDefault() {
-    const tsProject = ts.createProject('tsconfig.json', {
-        "emitDeclarationOnly": true
-    });
+    const tsProject = ts.createProject('tsconfig.json', tsOptions);
     return src('src/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(tsProject())
@@ -39,4 +41,34 @@ function exportDefault() {
         .pipe(dest('dist/'));
 }
 
-exports.default = parallel(exportDefault, exportModules);
+function exportModulesBundles() {
+    const tsProject = ts.createProject('tsconfig.json', tsOptions);
+    return src('src/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(babel({
+            presets: [['@babel/preset-env', {
+                targets: {"esmodules": true}
+            }]]
+        }))
+        .pipe(concat('index.bundle.js'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.esm.js' }))
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist/esm/'));
+}
+
+function exportDefaultBundles() {
+    const tsProject = ts.createProject('tsconfig.json', tsOptions);
+    return src('src/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(babel())
+        .pipe(concat('index.bundle.js'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist/'));
+}
+
+exports.default = parallel(series(exportDefault, exportDefaultBundles), series( exportModules, exportModulesBundles));
