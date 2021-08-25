b8d396a10926e6edeec4f5a185b8d0d19b8c6610
tvenceslau <email@email.com>
2021-08-26 00:05:39 +0100

    after release preparation

--- src/Model/Registry.ts
+++ src/Model/Registry.ts
@@ -4,9 +4,18 @@ import ModelKeys from "./constants";
 
 /**
  * Util class to enable serialization and correct rebuilding
+ * @param {string} anchorKey defaults to {@link ModelKeys.ANCHOR}. The property name where the registered class name is stored;
+ * @param {function(obj: {}): boolean} testFunction method to test if the provided object is a Model Object. defaults to {@link isModel}
  */
-class ModelRegistryManager {
+export class ModelRegistryManager {
   private cache: {[indexer: string]: any} = {};
+  private readonly testFunction: (obj: {}) => boolean;
+  private readonly anchorKey: string;
+
+  constructor(anchorKey: string = ModelKeys.ANCHOR, testFunction: (obj: {}) => boolean = isModel){
+      this.testFunction = testFunction;
+      this.anchorKey = anchorKey;
+  }
 
   register(name: string, constructor: any): void {
       if (!name || typeof constructor !== 'function')
@@ -14,10 +23,10 @@ class ModelRegistryManager {
       this.cache[name] = constructor;
   }
 
-  build<T extends Model>(obj: {[indexer: string]: any} = {}): any {
-      if (!isModel(obj))
+  build<T extends Model>(obj: {[indexer: string]: any} = {}): T {
+      if (!this.testFunction(obj))
           throw new Error(`Provided obj is not a Model object`);
-      const name = obj[ModelKeys.ANCHOR].class;
+      const name = obj[this.anchorKey].class;
       return new this.cache[name](obj);
   }
 }
