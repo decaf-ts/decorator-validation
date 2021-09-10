450cabb43104e787e036fc94a4a5e2a6392e2e58
tvenceslau <email@email.com>
2021-09-10 11:17:56 +0100

    refs #1 - update

--- src/validation/Validators/index.ts
+++ src/validation/Validators/index.ts
@@ -12,4 +12,5 @@ export * as MinLengthValidator from './MinLengthValidator';
 export * as MinValidator from './MinValidator';
 export * as PatternValidator from './PatternValidator';
 export * as URLValidator from './URLValidator';
+export * as StepValidator from './StepValidator';
 export * as TypeValidator from './TypeValidator';
