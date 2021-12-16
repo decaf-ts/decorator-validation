e257df43d0bafbfa09892969d6f66137af45e9a6
tvenceslau <email@email.com>
2021-12-16 17:34:31 +0000

    refs #1 - corrects build process. now loads without errors on web and still runs in node

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,7 @@
 /**
- * @namespace decorator-validation.utils.constants
- * @memberOf decorator-validation.utils
- */
-
-/**
  * Can be changed for localization purposes
  *
  * @constant MONTH_NAMES
- * @memberOf decorator-validation.utils.constants
  */
 export const MONTH_NAMES = [
     "January", "February", "March", "April", "May", "June", "July",
@@ -18,7 +12,6 @@ export const MONTH_NAMES = [
  * Can be changed for localization purposes
  *
  * @constant DAYS_OF_WEEK_NAMES
- * @memberOf decorator-validation.utils.constants
  */
 export const DAYS_OF_WEEK_NAMES = [
     "Sunday", "Monday", "Tuesday",
