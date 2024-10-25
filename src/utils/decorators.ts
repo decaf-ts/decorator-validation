54707d1bd8fa0bc0f5c3255cf8b4a3370712bb4a
Tiago <venceslau.tiago@gmail.com>
2024-10-25 21:10:32 +0100

    refs #1 - makes prop versatile to add behabiour using this mechanism

--- src/utils/decorators.ts
+++ src/utils/decorators.ts
@@ -1,13 +1,13 @@
 import { apply, metadata } from "@decaf-ts/reflection";
 import { ModelKeys } from "./constants";
 
-export function prop(): PropertyDecorator {
+export function prop(key: string = ModelKeys.ATTRIBUTE): PropertyDecorator {
   return (model: object, propertyKey: string | symbol): void => {
     let props: string[];
-    if (Object.prototype.hasOwnProperty.call(model, ModelKeys.ATTRIBUTE)) {
-      props = (model as any)[ModelKeys.ATTRIBUTE];
+    if (Object.prototype.hasOwnProperty.call(model, key)) {
+      props = (model as any)[key];
     } else {
-      props = (model as any)[ModelKeys.ATTRIBUTE] = [];
+      props = (model as any)[key] = [];
     }
     if (!props.includes(propertyKey as string))
       props.push(propertyKey as string);
