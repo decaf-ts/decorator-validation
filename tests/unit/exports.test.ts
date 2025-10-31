2ea4330b452f2a6b3d0fdd53480d8f1866dc1a11
tvenceslau <venceslau.tiago@gmail.com>
2025-10-31 10:26:01 +0000

    DECAF-178 debugging import problem

---
+++ tests/unit/exports.test.ts
@@ -0,0 +1,7 @@
+describe("lib import tests", () => {
+  it("should import all modules without errors", () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const a = require("../../lib/index.cjs");
+    expect(a).toBeDefined();
+  });
+});
