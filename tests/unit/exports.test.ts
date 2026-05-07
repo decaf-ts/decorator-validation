d12da0672ef497dbfebed2b312396eb3ff2db150
tvenceslau <venceslau.tiago@gmail.com>
2026-05-07 17:05:07 +0100

    minor - DECAF-734 build mode-no-ci - after release preparation

--- tests/unit/exports.test.ts
+++ tests/unit/exports.test.ts
@@ -1,7 +1,7 @@
 describe("lib import tests", () => {
   it("should import all modules without errors", () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const a = require("../../lib/index.cjs");
+    const a = require("../../lib/cjs/index.cjs");
     expect(a).toBeDefined();
     const { required } = a;
     expect(required).toBeDefined();
