9fee915aa8d45a7e59b869add35f1827bce97d68
tvenceslau <venceslau.tiago@gmail.com>
2026-05-25 18:43:36 +0100

    DECAF-798 - version

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,23 @@ export * from "./model";
 export const VERSION = "##VERSION##";
 
 /**
+ * @description Represents the current commit hash of the module build.
+ * @summary Stores the current git commit hash for the package. The build replaces
+ * the placeholder with the actual commit hash at publish time.
+ * @const COMMIT
+ */
+export const COMMIT = "##COMMIT##";
+
+/**
+ * @description Represents the full version string of the module.
+ * @summary Stores the semver version and commit hash for the package.
+ * The build replaces the placeholder with the actual `<version>-<commit>` value at publish time.
+ * @const FULL_VERSION
+ */
+export const FULL_VERSION = "##FULL_VERSION##";
+
+
+/**
  * @description Current version of the reflection package
  * @summary Stores the semantic version number of the package
  * @const PACKAGE_NAME
